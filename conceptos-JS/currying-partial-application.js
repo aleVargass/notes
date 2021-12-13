4074f8ecac0913830b8b66ed25c0b6ca34dbb34d
Ale Vargas <vargasalejandro2506@gmail.com>
2021-12-13 12:24:50 -0600

    currying made

---
+++ conceptos-JS/currying-partial-application.js
@@ -0,0 +1,27 @@
+function unCurried(x, y) {
+  return x + y;
+}
+
+function curried(x) {
+  return function(y) {
+    return x + y;
+  }
+}
+
+const curried = x => y => x + y
+
+curried(1)(2)
+
+
+
+const funcForY = curried(1);
+console.log(funcForY(2)); // 3
+
+
+
+function impartial(x, y, z) {
+  return x + y + z;
+}
+
+const partialFn = impartial.bind(this, 1, 2);
+partialFn(10); // 13
